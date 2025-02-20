6641ad1f605e7f51d52ebe34e72f1409f36868e3
liang zhuohua <110341664+liangzhuohua@users.noreply.github.com>
2025-02-20 21:44:19 +0800

    Update Bam.py

--- yolov5/models/Bam.py
+++ yolov5/models/Bam.py
@@ -4,25 +4,25 @@ class Bam(nn.Module):
     def __init__(self, in_channels, reduction_ratio = 16):
         super(Bam, self).__init__()
 
-        # 瓶颈结构中的压缩层
+        # Compression layer in the bottleneck structure
         self.compress = nn.Conv2d(in_channels, in_channels // reduction_ratio, kernel_size=1)
         self.relu = nn.ReLU()
 
-        # 瓶颈结构中的注意力机制
+        # The attention mechanism in bottleneck structure
         self.conv_atten = nn.Conv2d(in_channels // reduction_ratio, 1, kernel_size=1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         batch_size, channels, width, height = x.size()
 
-        # 压缩部分
-        x_compress = self.compress(x)  # 使用1x1卷积进行通道压缩
-        x_compress = self.relu(x_compress)  # ReLU激活函数
+        # Compressed portion
+        x_compress = self.compress(x)  # Perform channel compression using 1x1 convolution.
+        x_compress = self.relu(x_compress)  # ReLU activation function
 
-        # 注意力部分
-        atten = self.conv_atten(x_compress)  # 利用1x1卷积生成注意力权重
-        atten = self.sigmoid(atten)  # 使用Sigmoid函数将注意力权重限制在0到1之间
+        # Attention section
+        atten = self.conv_atten(x_compress)  # Generate attention weights by using 1x1 convolution.
+        atten = self.sigmoid(atten)  # Limit the attention weights within the range of 0 to 1 by using the Sigmoid function.
 
-        # 对输入特征加权融合得到最终输出
-        out = x * atten  # 利用注意力权重对特征图进行加权融合
+        # The final output is obtained by weighted fusion of the input features.
+        out = x * atten  # Weighted fusion of feature maps is carried out by utilizing attention weights.
         return out
