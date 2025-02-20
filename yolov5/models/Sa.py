41290fc5194b390615ab926a114dce338b1ae603
liang zhuohua <110341664+liangzhuohua@users.noreply.github.com>
2025-02-20 21:42:53 +0800

    Update Sa.py

--- yolov5/models/Sa.py
+++ yolov5/models/Sa.py
@@ -8,30 +8,30 @@ class Sa(nn.Module):
     def __init__(self, in_channels):
         super(Sa, self).__init__()
 
-        # 初始化查询、键、值的卷积层
+        # Initialization query, key, and value convolutional layer
         self.query_conv = nn.Conv2d(in_channels, in_channels // 8, kernel_size=1)
         self.key_conv = nn.Conv2d(in_channels, in_channels // 8, kernel_size=1)
         self.value_conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)
 
-        # 初始化注意力权重调节参数
+        # Initialize the parameters for adjusting the attention weights.
         self.gamma = nn.Parameter(torch.zeros(1))
 
     def forward(self, x):
         batch_size, channels, width, height = x.size()
 
-        # 计算查询、键、值
+        # Calculate queries, keys, and values
         proj_query = self.query_conv(x).view(batch_size, -1, width * height).permute(0, 2, 1)
         proj_key = self.key_conv(x).view(batch_size, -1, width * height)
 
-        # 计算注意力能量和注意力权重
+        # Calculate attention energy and attention weights
         energy = torch.bmm(proj_query, proj_key)
         attention = F.softmax(energy, dim=-1)
 
-        # 计算自注意力向量
+        # Calculate the self-attention vector
         proj_value = self.value_conv(x).view(batch_size, -1, width * height)
         out = torch.bmm(proj_value, attention.permute(0, 2, 1))
         out = out.view(batch_size, channels, width, height)
 
-        # 将自注意力向量与原始特征相加并乘以注意力权重调节参数
+        # Add the self-attention vector to the original features and multiply them by the attention weight adjustment parameters.
         out = self.gamma * out + x
         return out
